b6fc1be073c10dc55d82b1e9f378503da657b70f
Chitranshoo Prakash <chitranshoo.prakash@jarvis.consulting>
2024-09-10 16:47:09 +0530

    like and dislike

--- src/components/content-card/ContentCard.jsx
+++ src/components/content-card/ContentCard.jsx
@@ -1,13 +1,15 @@
 import classes from "./ContentCard.module.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import CustomModal from "../custom-modal/CustomModal.jsx";
 import VideoPlayer from "../video-player/VideoPlayer.jsx";
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import {IconButton} from "@mui/material";
-import {useMutation} from "@tanstack/react-query";
+import {performAction} from "../../api/videosApi.js";
+import {useSelector} from "react-redux";
 
 export default function ContentCard({ video }) {
+    const loggedInUser = useSelector(state => state.authData.user);
     const [isOpen, setIsOpen] = useState(false);
     const [isLiked, setIsLiked] = useState(false);
     const [isDisliked, setIsDisliked] = useState(false);
@@ -23,6 +25,38 @@ export default function ContentCard({ video }) {
     const handleClickCard = () => {
         handleOpen();
     }
+
+    const handleAction = (type, action) => {
+        const data = {
+            videoId: video._id,
+            isLike: type,
+            action: action
+        };
+        performAction(data).then(response => {
+            console.log(response.data);
+        }).catch(err => {
+            console.error(err.response.data);
+        });
+    };
+
+    useEffect(() => {
+        if (video.dislikes.length > 0) {
+            for (const ele of video.dislikes) {
+                if (ele.toString() === loggedInUser.id) {
+                    setIsDisliked(true);
+                    break;
+                }
+            }
+        }
+        if (video.likes.length > 0) {
+            for (const ele of video.likes) {
+                if (ele.toString() === loggedInUser.id) {
+                    setIsLiked(true);
+                    break;
+                }
+            }
+        }
+    }, []);
 
     return (
         <>
@@ -48,7 +82,11 @@ export default function ContentCard({ video }) {
                     <div className={`d-flex gap-2 mt-2`}>
                         <IconButton
                             disabled={isDisliked}
-                            onClick={() => setIsLiked(prevState => !prevState)}
+                            onClick={() => {
+                                const action = isLiked ? 'REMOVE' : 'ADD';
+                                handleAction(true, action);
+                                setIsLiked(prevState => !prevState);
+                            }}
                         >
                             <ThumbUpIcon
                                 color={isLiked ? 'success' : ''}
@@ -56,7 +94,11 @@ export default function ContentCard({ video }) {
                         </IconButton>
                         <IconButton
                             disabled={isLiked}
-                            onClick={() => setIsDisliked(prevState => !prevState)}
+                            onClick={() => {
+                                const action = isDisliked ? 'REMOVE' : 'ADD';
+                                handleAction(false, action);
+                                setIsDisliked(prevState => !prevState)
+                            }}
                         >
                             <ThumbDownIcon
                                 color={isDisliked ? 'error' : ''}
